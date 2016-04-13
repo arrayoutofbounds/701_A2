16ec533668e6644494ac9050c7c687a0b409115c
Anmol Desai <anmoldesai4@gmail.com>
2016-04-13 22:42:08 +1200

    forwared referencing caught by setting type in variable declaration expression in the definition visitor and then checking the line number of the RHS and LHS in the resolving visitor

--- src/symtab/VariableSymbol.java
+++ src/symtab/VariableSymbol.java
@@ -1,9 +1,19 @@
 package symtab;
 
 public class VariableSymbol extends Symbol {
+	
+	private int lineNumber;
 
 	public VariableSymbol(String name, Type type) {
 		super(name, type);
+	}
+	
+	public void setLineNumber(int line) {
+		this.lineNumber = line;
+	}
+	
+	public int getLineNumber() {
+		return this.lineNumber;
 	}
 
 }
